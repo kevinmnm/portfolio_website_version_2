8c6a8b00d8ab05d47210759fff70da85542ac2ec
Kevin Song <kevinmnm2010@gmail.com>
2020-09-21 13:15:05 -0400

    add new project gsg website built with react

--- projects/GSG/service-worker.js
+++ projects/GSG/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be782e87ae6bf5701dc287f97dd83266.js"
+  "/projects/GSG/precache-manifest.1419dfc9f9439ca6ab5adb649b79cda3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/GSG/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
