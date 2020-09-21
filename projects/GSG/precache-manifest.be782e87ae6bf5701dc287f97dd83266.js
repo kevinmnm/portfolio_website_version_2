3e8be7ac6dbe7cc3fcf383a44f622b3c8032c6ae
Kevin Song <kevinmnm2010@gmail.com>
2020-09-21 11:11:56 -0400

    added new project build with react.js - GSG website

---
+++ projects/GSG/precache-manifest.be782e87ae6bf5701dc287f97dd83266.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fb4d0192285667c532fb0ec15bab61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2612dbe6a0dc3e97f62c",
+    "url": "/static/css/3.984a5d6a.chunk.css"
+  },
+  {
+    "revision": "b4e36a49eb4f4d78f4a4",
+    "url": "/static/css/4.dd8086ff.chunk.css"
+  },
+  {
+    "revision": "45dd33f2d38fe42241b4",
+    "url": "/static/css/5.384ff09e.chunk.css"
+  },
+  {
+    "revision": "59de3d43f167bbaaaefa",
+    "url": "/static/css/6.ca6dcd9c.chunk.css"
+  },
+  {
+    "revision": "5ac5313078c51773b422",
+    "url": "/static/css/7.11d0198c.chunk.css"
+  },
+  {
+    "revision": "3addd672c91913e533ea",
+    "url": "/static/css/8.6f813456.chunk.css"
+  },
+  {
+    "revision": "917fd7a9ff829ce14cb4",
+    "url": "/static/css/main.a714a6ca.chunk.css"
+  },
+  {
+    "revision": "359411ea618f29759b01",
+    "url": "/static/js/2.27dd9315.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.27dd9315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2612dbe6a0dc3e97f62c",
+    "url": "/static/js/3.5ac58705.chunk.js"
+  },
+  {
+    "revision": "b4e36a49eb4f4d78f4a4",
+    "url": "/static/js/4.0261e502.chunk.js"
+  },
+  {
+    "revision": "45dd33f2d38fe42241b4",
+    "url": "/static/js/5.3462df62.chunk.js"
+  },
+  {
+    "revision": "59de3d43f167bbaaaefa",
+    "url": "/static/js/6.087eeab8.chunk.js"
+  },
+  {
+    "revision": "5ac5313078c51773b422",
+    "url": "/static/js/7.7b0e10dc.chunk.js"
+  },
+  {
+    "revision": "3addd672c91913e533ea",
+    "url": "/static/js/8.586aeaa8.chunk.js"
+  },
+  {
+    "revision": "917fd7a9ff829ce14cb4",
+    "url": "/static/js/main.c3145dfb.chunk.js"
+  },
+  {
+    "revision": "eed9b0c7bcb5149c8d9f",
+    "url": "/static/js/runtime-main.3f71ecaa.js"
+  },
+  {
+    "revision": "6435269d6c16e1a9555a8efacdf2c407",
+    "url": "/static/media/404.6435269d.png"
+  },
+  {
+    "revision": "ebb6f6d16356bd23edffa745b4bba17b",
+    "url": "/static/media/CPU_img.ebb6f6d1.jpg"
+  },
+  {
+    "revision": "13e27cce63374a4275def44ffd9168cc",
+    "url": "/static/media/dark_background.13e27cce.jpg"
+  },
+  {
+    "revision": "cfe988fdf3cc7a7b832498d638a5d410",
+    "url": "/static/media/game_fire_img.cfe988fd.png"
+  },
+  {
+    "revision": "e5109c0022bcb6989531ea6305a1671b",
+    "url": "/static/media/graphics_img.e5109c00.jpg"
+  },
+  {
+    "revision": "473434cd2c3ed3f110c17473a235a858",
+    "url": "/static/media/home_large_1.473434cd.png"
+  },
+  {
+    "revision": "1db3939cba5ae0bbc7c25cda4973ce88",
+    "url": "/static/media/home_large_2.1db3939c.png"
+  },
+  {
+    "revision": "be4a033b8763e5a2ffb6f28960a5402d",
+    "url": "/static/media/home_large_3.be4a033b.png"
+  },
+  {
+    "revision": "21152ab4c4fa44820c5b47cf9bdb08e3",
+    "url": "/static/media/home_large_4.21152ab4.png"
+  },
+  {
+    "revision": "7619bb01226584b49ddfedddedd1c139",
+    "url": "/static/media/logo.7619bb01.png"
+  },
+  {
+    "revision": "c2d538f464058cc81c397607ed99c221",
+    "url": "/static/media/motherboard_img.c2d538f4.jpg"
+  },
+  {
+    "revision": "cadc09c9ab462d90857bd505ab960a0e",
+    "url": "/static/media/razer_cortex_img.cadc09c9.png"
+  },
+  {
+    "revision": "3379b31a3ca034f1d2c4392843053f47",
+    "url": "/static/media/wise_game_img.3379b31a.png"
+  }
+]);
