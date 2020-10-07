c4862701711968147b9382a98b339bbda47f7a8b
Kevin Song <kevinmnm2010@gmail.com>
2020-10-07 12:58:07 -0400

    Temporarily disabled marquee element if its not chrome brower

--- new.js
+++ new.js
@@ -263,6 +263,34 @@ const myself = new Vue({
     }
 });
 
+// Detect browser type.
+
+   // Opera 8.0+
+   var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+   // Firefox 1.0+
+   var isFirefox = typeof InstallTrigger !== 'undefined';
+
+   // Safari 3.0+ "[object HTMLElementConstructor]" 
+   var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+   // Internet Explorer 6-11
+   var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+   // Edge 20+
+   var isEdge = !isIE && !!window.StyleMedia;
+
+   // Chrome 1 - 79
+   var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+   // Edge (based on chromium) detection
+   var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+   // Blink engine detection
+   var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+   let marq = document.querySelector('#marq');
+
 const sideR = new Vue({
     el: '#sideR',
     data: {
@@ -275,6 +303,11 @@ const sideR = new Vue({
         starti: function(){
             document.querySelector('#marq').start();
         }
+    },
+    mounted(){
+      if (!isChrome){
+         this.text = '';
+      }
     }
 });
 
@@ -745,3 +778,4 @@ last_navItem.addEventListener('animationend', () => {
       glowers[i].classList.add('glow-horizon');
    }
 });
+
